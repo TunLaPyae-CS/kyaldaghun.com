cfd7a109824aea72f65e89bc043bce96443a736e
Ye Kyaw Thu <yekyawthu@gmail.com>
2020-04-01 19:43:48 +0630

    Del db2

--- public_html/db_connect2.php
+++
@@ -1,4 +0,0 @@
-<?php
-$mysqli = new mysqli('localhost','root', '','myatchel_ytvideos') or die(mysqli_error($mysqli));
-//$mysqli = new mysqli('10.148.0.6','myatchel_ytvideos', 'vv0nd3R*fuL','myatchel_ytvideos') or die(mysqli_error($mysqli));
-?>
